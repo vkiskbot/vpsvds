15078c88d80aa59cb78ea8ff61b483e1fc0453fa
vkiskbot <53118918+vkiskbot@users.noreply.github.com>
2019-07-28 08:29:29 +0300

    Add files via upload

---
+++ precache-manifest.d1d805643a965ad9d2ffdbd57ac06ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d036f58772e6ab689073",
+    "url": "/vpsvds/static/js/runtime~main.d4594904.js"
+  },
+  {
+    "revision": "309c4246e3defb85ac16",
+    "url": "/vpsvds/static/js/main.38764f18.chunk.js"
+  },
+  {
+    "revision": "59e20cc28a39dceedb78",
+    "url": "/vpsvds/static/js/2.0b2ce6a2.chunk.js"
+  },
+  {
+    "revision": "309c4246e3defb85ac16",
+    "url": "/vpsvds/static/css/main.2e2fa2af.chunk.css"
+  },
+  {
+    "revision": "59e20cc28a39dceedb78",
+    "url": "/vpsvds/static/css/2.1760d7fa.chunk.css"
+  },
+  {
+    "revision": "6cf0037f7592e3308b9b18321ee507d6",
+    "url": "/vpsvds/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1748d423e9780a59bcfc1c893a29a082.js"
+  "/vpsvds/precache-manifest.d1d805643a965ad9d2ffdbd57ac06ce6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/vpsvds/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
